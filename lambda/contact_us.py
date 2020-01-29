e400e65fd6f6820c04ebfddc0a76849f27530a95
Alex <shaftoe@users.noreply.github.com>
2020-01-29 21:23:50 +0300

    Refactor contact_us response object

--- lambda/contact_us.py
+++ lambda/contact_us.py
@@ -5,19 +5,27 @@ import urllib.request
 from urllib.parse import urlencode
 
 LOG = logging.getLogger()
-LOG.setLevel('DEBUG')
+LOG.setLevel("DEBUG")
 
 
-def send_msg_to_pushover(token, userkey, url, title, payload):
+class HandledError(BaseException):
+    pass
+
+
+def send_msg_to_pushover(title, payload):
     """Send payload as message to Pushover API."""
+    token = environ["PUSHOVER_TOKEN"]
+    userkey = environ["PUSHOVER_USERKEY"]
+    url = environ["PUSHOVER_API_ENDPOINT"]
+
     data = urlencode({
-        'token': token,
-        'user': userkey,
-        'message': payload,
-        'title': title,
+        "token": token,
+        "user": userkey,
+        "message": payload,
+        "title": title,
     }).encode()
 
-    req = urllib.request.Request(url=url, data=data, method='POST')
+    req = urllib.request.Request(url=url, data=data, method="POST")
     return urllib.request.urlopen(req)
 
 
@@ -26,72 +34,59 @@ def contact_us(body):
 
     try:
         body = json.loads(body)
-        msg = 'Name: %(name)s\nMail: %(email)s\nDesc: %(description)s\n' % body
+        msg = "Name: %(name)s\nMail: %(email)s\nDesc: %(description)s\n" % body
 
     except (TypeError, json.JSONDecodeError) as error:
-        msg = 'JSON body is malformatted: %s' % error
-        LOG.fatal(msg)
-        return json.dumps({"error": msg}), 400
+        raise HandledError("JSON body is malformatted: %s" % error)
 
     except KeyError as error:
-        msg = 'Key missing: %s' % error
-        LOG.fatal(msg)
-        return json.dumps({'error': msg}), 400
+        raise HandledError("Key missing: %s" % error)
 
-    token = environ.get('PUSHOVER_TOKEN')
-    userkey = environ.get('PUSHOVER_USERKEY')
-    url = environ.get('PUSHOVER_API_ENDPOINT')
-
-    if not (token and userkey and url):
-        msg = 'Missing Pushover enviroment variables'
-        LOG.fatal(msg)
-        return json.dumps({'error': msg}), 500
-
-    try:
-        LOG.info('Delivering message via Pushover')
-        send_msg_to_pushover(token=token,
-                             userkey=userkey,
-                             url=url,
-                             title='New /contact_us submission received',
-                             payload=msg)
-        LOG.info('Message delivered successfully')
-        return json.dumps({"message": "message delivered"}), 200
-
-    except Exception as error:
-        LOG.fatal('Something went wrong: %s' % error)
-        return json.dumps({'error': str(error)}), 500
+    LOG.info("Delivering message via Pushover")
+    send_msg_to_pushover(title="New /contact_us submission received",
+                         payload=msg)
+    LOG.info("Message delivered successfully")
+    return "message delivered"
 
 
 def handler(event, context):
     # ref: https://docs.aws.amazon.com/apigateway/latest
     #   /developerguide/set-up-lambda-proxy-integrations.html
-    response = {
-        "body": json.dumps({"message": ""}),
-        "headers": {
-            "Access-Control-Allow-Origin": "*"
-        },
-        "statusCode": 405,
-        "isBase64Encoded": False,
-    }
+    class Response:
+        def __init__(self):
+            self.statusCode = 500
+            self.message = ""
+            self.error = None
 
-    path, method = event.get('path'), event.get('httpMethod')
-    body = event.get('body')
+        def out(self):
+            return {
+                "isBase64Encoded": False,
+                "statusCode": self.statusCode,
+                "headers": {"Access-Control-Allow-Origin": environ.get("CORS_ALLOW_ORIGIN", "*")},
+                "body": json.dumps({"error": str(self.error)}
+                                    if self.error else {"message": self.message}),
+            }
 
-    if not (path and method):
-        msg = "Missing API Gateway event data"
-        LOG.fatal(msg)
-        response["body"] = json.dumps({"error": msg})
-        return response
+    response = Response()
 
-    LOG.info('Received HTTP %s request for path %s' % (method, path))
+    try:
+        path, method = event["path"], event["httpMethod"]
+        LOG.info("Received HTTP %s request for path %s" % (method, path))
 
-    if (path == '/contact_us' and method == 'POST'):
-        response["body"], response["statusCode"] = contact_us(body)
+        if (path == "/contact_us" and method == "POST"):
+            response.message = contact_us(event.get("body"))
+            response.statusCode = 200
 
-    else:
-        msg = '%s %s not allowed' % (method, path)
-        response["statusCode"] = 405
-        response["body"] = json.dumps({"error": msg})
-        LOG.error(msg)
+        else:
+            raise HandledError("%s %s not allowed" % (method, path))
 
-    return response
+    except HandledError as error:
+        LOG.error(error)
+        response.error = error
+        response.statusCode = 405
+
+    except Exception as error:
+        response.error = error
+        LOG.fatal(error)
+
+    return response.out()
